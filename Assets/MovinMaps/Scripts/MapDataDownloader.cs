eacd5db78b80dc3c032102bc40bdf2550eba4c35
BrinkerVII <brinkervii@gmail.com>
2017-06-23 14:11:18 +0200

    Commit before i break my work

---
+++ Assets/MovinMaps/Scripts/MapDataDownloader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDataDownloader : MonoBehaviour
+{
+    public delegate void MapDataReceiver(MovinMap map);
+
+    [SerializeField]
+    public string MapURL = "https://kasiusbananavijfminutenkak.movin.io/data/maps/getmap?mapid=579226d5bcab1ba605c5e426&apikey=594cbb69063c404b2f24d952&includebuildings=true&includeentities=true";
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator Download(MapDataReceiver receiver)
+    {
+        Debug.Log("downloading things");
+        WWW www = new WWW(this.MapURL);
+        yield return www;
+
+        SimpleJSON.JSONNode json = SimpleJSON.JSON.Parse(www.text);
+        receiver(JsonUtility.FromJson<GetMapResponse>(www.text).map);
+    }
+}
+
+[Serializable]
+public class MovinMap
+{
+    public string _id;
+    public int __v;
+    public MovinBuilding[] buildings;
+}
+
+[Serializable]
+public class MovinBuilding
+{
+    public string _id;
+    public string type;
+    public int __v;
+    public MovinEntity[] entities;
+}
+
+[Serializable]
+public class MovinEntityProperties
+{
+    public string name;
+    public string baseType;
+    public string subType;
+    public int flooar;
+    public string buildingId;
+    public string mapID;
+    public int rotation;
+}
+
+[Serializable]
+public class MovinEntityGeometry
+{
+    public string type;
+    public decimal[][][] coordinates;
+}
+
+[Serializable]
+public class MovinEntity
+{
+    public string _id;
+    public string type;
+    public int __v;
+    public MovinEntityGeometry geometry;
+    public MovinEntityProperties properties;
+}
+
+[Serializable]
+public class GetMapResponse
+{
+    public Boolean succes;
+    public MovinMap map;
+}
---
+++ Assets/MovinMaps/Scripts/MapMeshGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMeshGenerator : MonoBehaviour
+{
+    MapDataDownloader downloader;
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] tris;
+
+    void Awake()
+    {
+        downloader = GetComponent<MapDataDownloader>();
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Running the map mesh generator :O");
+        StartCoroutine(downloader.Download(this.DataReceiver));
+    }
+
+    private void CreateMesh()
+    {
+        Debug.Log("Creating mesh");
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+    }
+
+    private void MakeMeshData(MovinBuilding building)
+    {
+        List<Vector3> v3List = new List<Vector3>();
+
+        foreach (MovinEntity entity in building.entities)
+        {
+            if (entity.geometry != null && entity.properties.baseType == "Room")
+            {
+                Debug.Log(entity.geometry.type);
+                Debug.Log(entity.geometry.coordinates);
+                //foreach (float[] coordinate in entity.geometry.coordinates[0])
+                //{
+                //    v3List.Add(new Vector3(coordinate[0], 0, coordinate[1]));
+                //}
+            }
+        }
+
+        this.tris = new int[v3List.Count];
+        this.vertices = v3List.ToArray();
+        for (int i = 0; i < this.vertices.Length; i++)
+        {
+            tris[i] = i;
+        }
+
+        Debug.Log("Made mesh data");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DataReceiver(MovinMap map)
+    {
+        Debug.Log(map.buildings[0]._id);
+
+        MakeMeshData(map.buildings[0]);
+        CreateMesh();
+    }
+}
